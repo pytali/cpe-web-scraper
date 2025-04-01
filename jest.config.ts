97f7e752f5e4ac83cb065b13bdc25b94c63c286d
Walisson Gois <walissongois@gmail.com>
2025-04-01 02:25:17 -0400

    test: ajusta configuração do Jest e adiciona setup de testes

--- jest.config.ts
+++ jest.config.ts
@@ -4,13 +4,15 @@ const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
         '^@/(.*)$': '<rootDir>/src/$1'
     },
     testMatch: [
         '**/__tests__/**/*.test.ts'
     ],
     setupFiles: [
-        'dotenv/config'
+        'dotenv/config',
+        '<rootDir>/src/config/__tests__/setup.ts'
     ],
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
     collectCoverage: true,
@@ -21,11 +23,13 @@ const config: Config.InitialOptions = {
         '/dist/',
         '/__tests__/'
     ],
-    globals: {
-        'ts-jest': {
+    transform: {
+        '^.+\\.ts$': ['ts-jest', {
+            useESM: true,
             tsconfig: 'tsconfig.json'
-        }
-    }
+        }]
+    },
+    extensionsToTreatAsEsm: ['.ts']
 }
 
 export default config; 
